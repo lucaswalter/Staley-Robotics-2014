f0bffd54fc0e07d7a948fc821c6bdce716f46ed5
Ian <Ian@10.9.146.245>
2014-01-15 17:57:53 -0600

    Added Sensitivity Functionality.

---
+++ src/org/usfirst/frc4959/StaleyRobotics2014/commands/DecreaseSensitivity.java
@@ -0,0 +1,47 @@
+package org.usfirst.frc4959.StaleyRobotics2014.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc4959.StaleyRobotics2014.Robot;
+
+/**
+ *
+ * @author Team Up Next
+ */
+public class DecreaseSensitivity extends Command
+    {
+
+    public DecreaseSensitivity()
+        {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        requires(Robot.driveTrain);
+        }
+
+    // Called just before this Command runs the first time
+    protected void initialize()
+        {
+        }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute()
+        {
+        Robot.driveTrain.decreaseSensitivity();
+        }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished()
+        {
+        return true;
+        }
+
+    // Called once after isFinished returns true
+    protected void end()
+        {
+        }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted()
+        {
+        }
+    }
