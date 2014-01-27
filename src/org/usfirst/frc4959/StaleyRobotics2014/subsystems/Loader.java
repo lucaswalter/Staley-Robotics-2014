844aa8fd60dab354c36d45cf8e9a0d51788d1b81
icedude10 <icedude10@ymail.com>
2014-01-27 16:27:54 -0600

    Update Loader.java

--- src/org/usfirst/frc4959/StaleyRobotics2014/subsystems/Loader.java
+++ src/org/usfirst/frc4959/StaleyRobotics2014/subsystems/Loader.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.usfirst.frc4959.StaleyRobotics2014.subsystems;
 
 import edu.wpi.first.wpilibj.SpeedController;
@@ -9,8 +5,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import org.usfirst.frc4959.StaleyRobotics2014.RobotMap;
 
 /**
- *
- * @author LW029415
+ * @author Ian
  */
 public class Loader extends Subsystem
     {
@@ -21,15 +16,16 @@ public class Loader extends Subsystem
         {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+        setDefaultCommand(new MoveLoader());
         }
     
     //Values may need to be reversed depending on the direction the motor turns
     public void moveLoader(double input)
         {
             if (input < 0) {
-                loaderVictor.set(0.5);
+                loaderVictor.set(-0.6);
             } else if (input > 0) {
-                loaderVictor.set(-0.5);
+                loaderVictor.set(0.6);
             } else {
                 loaderVictor.set(0.0);
             }
