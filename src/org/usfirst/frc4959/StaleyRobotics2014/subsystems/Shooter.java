c7a2b7bb782caf503cdfaa78daae05c047cd787b
lucaswalter <lucaswyland11@gmail.com>
2014-01-31 16:15:18 -0600

    Merged Autonomous Changes.

--- src/org/usfirst/frc4959/StaleyRobotics2014/subsystems/Shooter.java
+++ src/org/usfirst/frc4959/StaleyRobotics2014/subsystems/Shooter.java
@@ -1,25 +1,31 @@
 package org.usfirst.frc4959.StaleyRobotics2014.subsystems;
 
 /**
- *
  * @author Saul and Dustin
  */
+
 import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.usfirst.frc4959.StaleyRobotics2014.RobotMap;
-import org.usfirst.frc4959.StaleyRobotics2014.commands.*;
-import edu.wpi.first.wpilibj.*;
 
 public class Shooter extends Subsystem
 {
+    
+    private Relay firingSpike = RobotMap.FiringSpike;
+    
      public void initDefaultCommand()
-        {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-        }
+     {
+         // Set the default command for a subsystem here.
+         //setDefaultCommand(new MySpecialCommand());
+     }
      
      public void fire()
      {
-         //Add firing code here
+         firingSpike.set(Relay.Value.kOn);
      }  
+     
+     public void reload()
+     {
+         firingSpike.set(Relay.Value.kOff);
+     }
 }
