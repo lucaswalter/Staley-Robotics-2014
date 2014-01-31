57387a10b49acd3f37f4effe6da5a02d961b476c
lucaswalter <lucaswyland11@gmail.com>
2014-01-31 16:33:07 -0600

    Mapped Firing Commands To Xbox Controlller.

--- src/org/usfirst/frc4959/StaleyRobotics2014/RobotMap.java
+++ src/org/usfirst/frc4959/StaleyRobotics2014/RobotMap.java
@@ -24,6 +24,8 @@ public class RobotMap
     //TODO: Rework controller bindings
     public static final int A_BUTTON = 1;
     public static final int B_BUTTON = 2;
+    public static final int X_BUTTON = 3;
+    public static final int Y_BUTTON = 4;
     public static final int LEFT_BUMPER = 5;
     public static final int RIGHT_BUMPER = 6;
     
